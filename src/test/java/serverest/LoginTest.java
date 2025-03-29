954b6d3211efa3fd2246211857f3899fcaec880d
edilsonjrcode <2022111510116@iesp.edu.br>
2025-03-29 15:20:37 -0300

    add: insert login tests

--- src/test/java/serverest/LoginTest.java
+++ src/test/java/serverest/LoginTest.java
@@ -1,33 +1,78 @@
 package serverest;
+
 import dto.LoginDTO;
 import io.restassured.http.ContentType;
+
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-public class LoginTest extends  BaseTest{
+public class LoginTest extends BaseTest {
 
     @Test
     public void deveFazerLoginComSucesso() {
+        LoginDTO loginDto = new LoginDTO("fulano@qa.com", "teste");
         given()
                 .contentType(ContentType.JSON)
-                .body(new LoginDTO("fulano@qa.com","teste"))
+                .body(loginDto)
                 .when()
                 .post("/login")
                 .then()
-                .statusCode(200)
+                .statusCode(HttpStatus.SC_OK)
                 .body("message", containsString("Login realizado com sucesso")).log().all();
     }
+
     @Test
-    public void deveFalharLoginComSenhaInvalida() {
+    public void deveFalharLoginComEmailEmBranco() {
+        LoginDTO loginComSenhaInvalida = new LoginDTO("", "senhaInvalida");
         given()
                 .contentType(ContentType.JSON)
-                .body("{\"email\":\"fulano@qa.com\",\"password\":\"senhaInvalida\"}")
+                .body(loginComSenhaInvalida)
                 .when()
                 .post("/login")
                 .then()
-                .statusCode(401)
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("email", containsString("email não pode ficar em branco"));
+    }
+
+    @Test
+    public void deveFalharLoginComSenhaEmBranco() {
+        LoginDTO loginComSenhaInvalida = new LoginDTO("fulano@qa.com", "");
+        given()
+                .contentType(ContentType.JSON)
+                .body(loginComSenhaInvalida)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("password", containsString("password não pode ficar em branco"));
+    }
+
+    @Test
+    public void deveFalharLoginComEmailInvalido() {
+        LoginDTO loginComSenhaInvalida = new LoginDTO("ciclano@qa.com", "senhaInvalida");
+        given()
+                .contentType(ContentType.JSON)
+                .body(loginComSenhaInvalida)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
+                .body("message", containsString("Email e/ou senha inválidos"));
+    }
+
+    @Test
+    public void deveFalharLoginComSenhaInvalida() {
+        LoginDTO loginComSenhaInvalida = new LoginDTO("fulano@qa.com", "senhaInvalida");
+        given()
+                .contentType(ContentType.JSON)
+                .body(loginComSenhaInvalida)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED)
                 .body("message", containsString("Email e/ou senha inválidos"));
     }
 }
