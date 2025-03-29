2c514cdb8c3bb9dd9d294b5b18d6f228396ffdb7
edilsonjrcode <2022111510116@iesp.edu.br>
2025-03-29 15:19:58 -0300

    fix: insert loginDTO in the function and remove no used imports

--- src/test/java/serverest/BaseTest.java
+++ src/test/java/serverest/BaseTest.java
@@ -7,23 +7,28 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import dto.LoginDTO;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
+
+import org.apache.http.HttpStatus;
 
 public class BaseTest {
     public static String TOKEN;
 
     @BeforeAll
     public static void setup() {
+        LoginDTO loginDto = new LoginDTO("fulano@qa.com", "teste");
         RestAssured.baseURI = "http://localhost:3000";
         TOKEN = given()
-                    .contentType(ContentType.JSON)
-                    .body("{\"email\":\"fulano@qa.com\",\"password\":\"teste\"}")
-                    .when()
-                    .post("/login")
-                    .then()
-                    .statusCode(200)
-                    .body("message", containsString("Login realizado com sucesso"))
+                .contentType(ContentType.JSON)
+                .body(loginDto)
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", containsString("Login realizado com sucesso"))
                 .extract().jsonPath().get("authorization");
     }
 }
