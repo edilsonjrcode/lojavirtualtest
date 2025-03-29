7272ac2d45a930887bf6ca5a77d49e022d43e703
edilsonjrcode <2022111510116@iesp.edu.br>
2025-03-29 15:20:45 -0300

    add: insert product tests

--- src/test/java/serverest/ProdutoTest.java
+++ src/test/java/serverest/ProdutoTest.java
@@ -1,11 +1,18 @@
 package serverest;
 
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
 import org.junit.jupiter.api.Test;
+
+import dto.ProdutoDTO;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-public class ProdutoTest extends BaseTest{
+
+import org.apache.http.HttpStatus;
+
+public class ProdutoTest extends BaseTest {
     @Test
     public void deveListarProdutos() {
         when()
@@ -27,5 +34,107 @@ public class ProdutoTest extends BaseTest{
                 .then()
                 .statusCode(401)
                 .body("message", containsString("Token de acesso ausente"));
+    }
+
+    @Test
+    public void naoDeveCadastrarProdutoComMesmoNome() {
+        ProdutoDTO produtoComMesmoNome = new ProdutoDTO("Logitech MX Vertical", 200, "Mouse", 5);
+
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .body(produtoComMesmoNome)
+                .when()
+                .post("/produtos")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", containsString("Já existe produto com esse nome"));
+    }
+
+    @Test
+    public void deveCadastrarProduto() {
+        ProdutoDTO produto = new ProdutoDTO();
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .body(produto)
+                .when()
+                .post("/produtos")
+                .then()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("message", containsString("Cadastro realizado com sucesso"));
+    }
+
+    @Test
+    public void deveEncontrarProdutoPeloId() {
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .params("_id", "BeeJh5lz3k6kSIzA")
+                .when()
+                .get("/produtos/")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("produtos[0]", hasKey("nome"))
+                .body("produtos[0]", hasKey("preco"))
+                .body("produtos[0]", hasKey("descricao"))
+                .body("produtos[0]", hasKey("quantidade"))
+                .body("produtos[0]", hasKey("_id"));
+    }
+
+    @Test
+    public void deveDeletarProdutoPeloId() {
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .params("_id", "BeeJh5lz3k6kSIzA")
+                .when()
+                .delete("/produtos/")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", containsString("Registro excluído com sucesso | Nenhum registro excluído"));
+
+    }
+
+    @Test
+    public void naoDeveDeletarProdutoPeloIdInexistente() {
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .params("_id", "eee")
+                .when()
+                .delete("/produtos/")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("message", containsString("Nenhum registro excluído"));
+
+    }
+
+    @Test
+    public void naoDeveEncontrarProdutoComIdInexistente() {
+        String id = "1d1n3x1st3nt3";
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .params("_id", id)
+                .when()
+                .get("/produtos/")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", containsString("Produto não encontrado"));
+    }
+
+    @Test
+    public void deveEditarProdutoPeloId() {
+        String id = "K6leHdftCeOJj8BJ";
+        given()
+                .header("Authorization", TOKEN)
+                .contentType(ContentType.JSON)
+                .params("_id", id)
+                .when()
+                .put("/produtos/")
+                .then()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("message", containsString("Produto não encontrado"));
     }
 }
